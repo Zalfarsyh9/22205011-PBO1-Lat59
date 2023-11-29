aef617c891efaeff4e765ec31bf0270833eab2f7
Zalfarsyh9 <145966652+Zalfarsyh9@users.noreply.github.com>
2023-11-29 13:29:41 +0700

    Add files via upload

---
+++ src/pkg22205011/pbo1/lat59/PBO1Lat59.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg22205011.pbo1.lat59;
+
+/**
+ *
+ * @author User
+ * Nama : Zalfa Rosiyah Riski 
+ * Nim : 22205011
+ * Jurusan : Teknik Informatika 
+ * Mata kuliah : PBO 1
+ * Deskripsi : membuat program g
+ */
+public class PBO1Lat59 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
